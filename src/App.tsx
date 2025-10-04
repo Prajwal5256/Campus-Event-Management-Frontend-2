1c18ce13e6b59525dfd5965a2d3c73b0b7ade446
harshbhosale25 <harshadbhosale25202@gmail.com>
2025-10-04 12:26:39 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function AppRoutes() {
 
   if (loading) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
+      <div className="max-h-screen flex items-center justify-center">
         <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-primary"></div>
       </div>
     );
